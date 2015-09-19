91e6f4a7d6e6a0cf294e741ebd092163a33f95e5
Carl Lee <ljbha007@gmail.com>
2015-09-19 20:48:28 +0000

    added changing channels

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,73 @@
 angular.module('starter.controllers', ['ionic'])
 
-.controller('ChatsCtrl', function($scope, $ionicScrollDelegate, $ionicPopup,
-    rfc4122, mqttClient, storage) {
+.controller('ChatsCtrl', function($scope, $ionicScrollDelegate, $ionicNavBarDelegate, 
+    $ionicLoading, $ionicPopover, rfc4122, mqttClient, storage) {
+  function getName(names){
+    if(names.zh != undefined && names.zh.length > 0){
+      return names.zh[0];
+    }else{
+      return names.name;
+    }
+  }
+  var defaultPoi = {
+    "_id": "55fd6ef026adfe2d07fda69f",
+    "names": {
+      "name": "Wangjing",
+      "zh": [
+        "望京",
+        "望京街道"
+      ]
+    },
+    "loc": {
+      "type": "Point",
+      "coordinates": [
+        116.47284,
+        39.9933
+      ]
+    },
+  };
+  var defaultChannel = 'general';
+  var poi = storage['selected_poi'] || defaultPoi;
+  var placeId = poi._id;
+  var placeTopic = "/places/" + placeId + "/";
+  var topic = placeTopic + defaultChannel;
+  $scope.title = getName(poi.names);
   $scope.msgs = [];
   $scope.images = {};
-  mqttClient.subscribe("chat");
+  $scope.channels = [
+    {
+      name: '#综合',
+      channel: 'general'
+    },{
+      name: '#交易',
+      channel: 'trade'
+    },{
+      name: '#交友',
+      channel: 'social'
+    },{
+      name: '#问答',
+      channel: 'qa'
+    },{
+      name: '#帮忙',
+      channel: 'help'
+    }
+  ];
+  mqttClient.subscribe(topic);
   mqttClient.on("message", function(topic, payload){
+    console.log(topic + ": " + payload);
     var msg = JSON.parse(payload);
     var msgType = msg['type'];
-    var image = msg['image'];
-    if(msgType == "image" && image != undefined){
+    var imageId = msg['image_id'];
+    // debugger;
+    $scope.images[imageId] = "/img/placeholder.png";
+    if(msgType == "image"){
+      var image = msg['image'];
+      $scope.$apply(function(){
+        $scope.images[imageId] = image;
+        $ionicScrollDelegate.$getByHandle('msg-list').scrollBottom(true);
+      });
       return;
     }
-    console.log(topic + ": " + payload);
     $scope.$apply(function(){
       $scope.msgs.push(msg);
       $ionicScrollDelegate.$getByHandle('msg-list').scrollBottom(true);
@@ -30,100 +85,154 @@ angular.module('starter.controllers', ['ionic'])
     msg['gender'] = localStorage['gender'];
     msg['user_id'] = localStorage['id'];
     msg['msg'] = $scope.messageToSend;
-    mqttClient.publish("chat", JSON.stringify(msg));
+    mqttClient.publish(topic, JSON.stringify(msg));
     $scope.messageToSend = "";
   };
   $scope.pickImage = function(){
     $('#imageFile').trigger('click');
+    $ionicLoading.show();
   }
   $scope.sendImage = function(e){
     e.preventDefault();
     e.stopPropagation();
+    $ionicLoading.hide();
     var input = $('#imageFile')[0];
     var file = input.files[0];
-    var reader = new FileReader();
     var msg = {};
     var imageId = rfc4122.v4();
-    msg['type'] = "image";
+    msg['type'] = "image_header";
     msg['face'] = localStorage['face'];
     msg['nickname'] = localStorage['nickname'];
     msg['gender'] = localStorage['gender'];
     msg['user_id'] = localStorage['id'];
     // msg['image'] = e.target.result;
     msg['image_id'] = imageId;
-    mqttClient.publish("chat", JSON.stringify(msg));
+    
+    console.log("sending image_header..");
+    mqttClient.publish(topic, JSON.stringify(msg));
+    var reader = new FileReader();
     reader.onload = function(e){
       var msg = {};
       msg['image_id'] = imageId;
       msg['image'] = e.target.result;
-      mqttClient.publish("chat", JSON.stringify(msg));
+      msg['type'] = 'image';
+      console.log("sending image..");
+      mqttClient.publish(topic, JSON.stringify(msg));
     }
     reader.readAsDataURL(file);
   }
-  
+  $ionicPopover.fromTemplateUrl("/templates/chat-popover.html", {
+    scope: $scope
+  }).then(function(popover){
+    $scope.popover = popover;
+  });
+  $scope.openPopover = function($event){
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function(){
+    $scope.popover.hide();
+  };
+  $scope.changeChannel = function(newChannel){
+    $scope.msgs.splice(0, $scope.msgs.length);
+    mqttClient.unsubscribe(topic);
+    topic = placeTopic + newChannel;
+    mqttClient.subscribe(topic);
+  }
+  $scope.changeChannelAndClose = function(newChannel){
+    $scope.changeChannel(newChannel);
+    $scope.closePopover();
+  }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, storage) {
   $scope.msg = storage[$stateParams.chatId];
 })
-.controller('MapCtrl', function($scope, $ionicLoading, $compile, $state) {
-    //    debugger;
-  //    function initialize() {      //
+.controller('MapCtrl', function($scope, $ionicLoading, $state, restService, storage) {
+  var infowindows = [];
+  function gotoChat(e){
+    var poi = e.target.poi;
+    console.log("gotoChat: ", poi);
+    $state.go('chats');
+    storage['selected_poi'] = poi;
+  }
+  function getName(names){
+    if(names.zh != undefined && names.zh.length > 0){
+      return names.zh[0];
+    }else{
+      return names.name;
+    }
+  }
+  function makeMarker(poi){
+    var latLng = poi.loc.coordinates;
+    var name = getName(poi.names);
+    var latLng = new google.maps.LatLng(latLng[1], latLng[0]);
+    
+    var contentDiv = document.createElement('div');
+    contentDiv.poi = poi;
+    contentDiv.innerHTML = name;
+    $(contentDiv).click(gotoChat);
+  
+    var infowindow = new google.maps.InfoWindow({
+      content: contentDiv
+    });
+    infowindows.push(infowindow);
+  
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      title: name
+    });
+  
+    google.maps.event.addListener(marker, 'click', function(){
+      infowindows.forEach(function(infoW){
+        infoW.close();
+      });
+      infowindow.open(map,marker);
+    });
+  }
+  restService.nearPlaces({
+    lat: 39.994669099999996,
+    lng: 116.4747621
+  }, function(res){
+    console.log(res);
+    var pois = res.data;
+    if(pois != undefined){
+      pois.forEach(makeMarker);
+    }
+  });
 
-        var myLatlng = new google.maps.LatLng(39.994669099999996,116.4747621);
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"),
-            mapOptions);
-        
-        var contentString = "<div><a ng-click='clickTest()'>进入聊天室!</a></div>";
-        var compiled = $compile(contentString)($scope);
+  var latLng = new google.maps.LatLng(39.994669099999996, 116.4747621);
+  var mapOptions = {
+    center: latLng,
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"),
+    mapOptions);
+  $scope.map = map;
+  //}
+//    google.maps.event.addDomListener(window, 'load', initialize);
+  
+  $scope.centerOnMe = function() {
+    if(!$scope.map) {
+      return;
+    }
 
-        var infowindow = new google.maps.InfoWindow({
-          content: compiled[0]
-        });
+    $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
 
-        var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
-
-        $scope.map = map;
-      //}
-  //    google.maps.event.addDomListener(window, 'load', initialize);
-      
-      $scope.centerOnMe = function() {
-        if(!$scope.map) {
-          return;
-        }
-
-        $scope.loading = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
-
-        navigator.geolocation.getCurrentPosition(function(pos) {
-          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-        //  debugger;
-          $scope.loading.hide();
-        }, function(error) {
-          alert('Unable to get location: ' + error.message);
-        });
-      };
-      
-      $scope.clickTest = function() {
-      $state.go('chats');
-      };
-      
-    })
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+    //  debugger;
+      $ionicLoading.hide();
+    }, function(error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+  
+})
 
 .controller('LgCtrl', function($scope, $state) {
     $scope.signIn = function(user) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,4 @@
 angular.module('starter.services', ['uuid'])
-
-.factory('mqttClient', function(){
-  var client = mqtt.connect("wss://hangout-carllee.c9.io"); 
-  return client;
-})
-.factory('storage', function(){
-  var storage = {};
-  return storage;
-})
 .directive('customOnChange', function() {
   return {
     restrict: 'A',
@@ -18,4 +9,27 @@ angular.module('starter.services', ['uuid'])
       $(element).change(scope.onChange);
     }
   };
+})
+
+.factory('mqttClient', function(){
+  var client = mqtt.connect("wss://hangout-carllee.c9.io"); 
+  return client;
+})
+.factory('storage', function(){
+  var storage = {};
+  return storage;
+})
+.constant('API_END_POINT', "https://hangout-carllee.c9.io")
+.factory('restService', function(API_END_POINT, $http){
+  var service = {};
+  service.nearPlaces = function(params, callback){
+    $http({
+      method: 'POST',
+      url: API_END_POINT + "/places/near",
+      data: $.param(params),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    })
+    .then(callback, callback);
+  }
+  return service;
 });
